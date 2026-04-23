90eed4d2ddeb437599e022fc634b1bfd7f4dfd48
Tom Smeets <tom@tsmeets.nl>
2026-04-23 23:18:19 +0200

    build: add highlights

--- build.sh
+++ build.sh
@@ -3,9 +3,13 @@ tree-sitter generate
 tree-sitter build
 
 # Install helix
-mkdir -p ~/.config/helix/runtime/grammars/
-cp parser.so ~/.config/helix/runtime/grammars/tom.so
+mkdir -p          ~/.config/helix/runtime/grammars/
+cp tom.so         ~/.config/helix/runtime/grammars/tom.so
+
+mkdir -p          ~/.config/helix/runtime/queries/tom/
+cp highlights.scm ~/.config/helix/runtime/queries/tom/highlights.scm
 
 # Example highligh
 tree-sitter parse     project.tom
 tree-sitter highlight project.tom
+hx --health tom
